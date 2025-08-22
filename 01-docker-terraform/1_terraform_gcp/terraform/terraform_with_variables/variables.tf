685107b5d2a4b226219b3441be1f39d3800b38b3
michaelbbyrne <mbbyrne@live.com>
2025-08-22 17:47:57 -0400

    revert

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "/Users/mikebyrne/.gcloud-keys/data-training-469720-21c3d82b0fab.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -8,7 +8,7 @@ variable "credentials" {
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "data-training-469720"
 }
 
 variable "region" {
@@ -32,7 +32,7 @@ variable "bq_dataset_name" {
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "datacamp-mb-bucket-1"
 }
 
 variable "gcs_storage_class" {
